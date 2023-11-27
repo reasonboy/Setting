9c0eaf2b56ac52756f21d533cdec8494c4805bd7
jay <jay@innowaveglobal.com>
2023-11-27 18:13:40 +0900

    [GSFID] Implement onClickGsfIdBtn method

--- app/src/main/java/com/jzzh/setting/device/GSFActivity.java
+++ app/src/main/java/com/jzzh/setting/device/GSFActivity.java
@@ -1,20 +1,33 @@
 package com.jzzh.setting.device;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jzzh.setting.BaseActivity;
 import com.jzzh.setting.R;
 
 public class GSFActivity extends BaseActivity implements View.OnClickListener {
-
+    private static final String TAG = GSFActivity.class.getSimpleName();
     private static final Uri sUri = Uri.parse("content://com.google.android.gsf.gservices");
+    public static final String gsf_reg_url = "https://www.google.com/android/uncertified/";
+    public static final String GOOGLE_PLAY = "com.android.vending";
+    public static final String GOOGLE_PLAY_MAIN = "com.android.vending.AssetBrowserActivity";
 
     private View mGsfContentLayout;
     private ImageView mGsfSwitch;
@@ -23,6 +36,8 @@ public class GSFActivity extends BaseActivity implements View.OnClickListener {
     private TextView mGsfIdState;
     private TextView mGsfIdBtn;
     private boolean mManualShow = false;
+
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +97,11 @@ public class GSFActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        if (dialog != null) {
+            dialog.dismiss();
+            dialog = null;
+        }
     }
 
     private void setGooglePlayEnable(int enable) {
@@ -126,7 +146,61 @@ public class GSFActivity extends BaseActivity implements View.OnClickListener {
             mManualShow = !mManualShow;
             updateManualState();
         } else if(view == mGsfIdBtn) {
-
+            onClickGsfIdBtn();
         }
+    }
+
+    private void onClickGsfIdBtn() {
+        String gsfKey = getGsfId();
+        updateGsfIdState();
+
+        if (!isWifiConnected()) {
+            Toast.makeText(this, R.string.msg_wifi_not_connected, Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.d(TAG, "gsfKey is empty");
+        if (gsfKey == null || gsfKey.isEmpty()) {
+            Log.d(TAG, "gsfKey is empty");
+            dialog = new AlertDialog.Builder(this)
+                    .setMessage(R.string.msg_gsf_id_empty)
+                    .setPositiveButton(R.string.confirm, (dialog, which) -> {
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        ComponentName cn = new ComponentName(GOOGLE_PLAY, GOOGLE_PLAY_MAIN);
+                        intent.setComponent(cn);
+                        startActivity(intent);
+                    })
+                    .show();
+            return;
+        }
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("gsfid", gsfKey);
+        if (clipboard != null) {
+            clipboard.setPrimaryClip(clip);
+        } else {
+            Log.e(TAG, "clipboard service is null!!");
+        }
+
+        Toast.makeText(this, R.string.toast_msg_gsf_select, Toast.LENGTH_LONG).show();
+
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(gsf_reg_url));
+            startActivity(intent);
+        } catch (Exception e) {
+            Log.e(TAG, "call web page failed!!");
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isWifiConnected() {
+        boolean result = false;
+        try {
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            result = cm != null && cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
