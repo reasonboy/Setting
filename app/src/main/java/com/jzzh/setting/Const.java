dd8a0c7b09d370210fc4a2065f1e0148987edd40
xuminglong <xuml@jezetek.hk>
2025-12-09 21:42:59 +0800

    [Power]Implement power manage

--- app/src/main/java/com/jzzh/setting/Const.java
+++ app/src/main/java/com/jzzh/setting/Const.java
@@ -31,6 +31,7 @@ import com.jzzh.setting.network.wifi.WifiActivity;
 import com.jzzh.setting.power.AutoPowerOffActivity;
 import com.jzzh.setting.power.AutoSleepActivity;
 import com.jzzh.setting.power.PowerActivity;
+import com.jzzh.setting.power.PowerManageActivity;
 import com.jzzh.setting.task.TaskManagerActivity;
 import com.jzzh.setting.time.DateAndTimeActivity;
 import com.jzzh.setting.time.TimeZoneActivity;
@@ -49,6 +50,7 @@ public class Const {
             PowerActivity.class,
             AutoSleepActivity.class,
             AutoPowerOffActivity.class,
+            PowerManageActivity.class,
 
             DisplayActivity.class,
             RefreshActivity.class,
@@ -97,6 +99,7 @@ public class Const {
             {R.string.setting,R.string.setting_power},
             {R.string.setting,R.string.setting_power,R.string.setting_power_auto_sleep},
             {R.string.setting,R.string.setting_power,R.string.setting_power_auto_power_off},
+            {R.string.setting,R.string.setting_power,R.string.setting_power_manage},
 
             {R.string.setting,R.string.setting_display},
             {R.string.setting,R.string.setting_display,R.string.setting_display_refresh},
