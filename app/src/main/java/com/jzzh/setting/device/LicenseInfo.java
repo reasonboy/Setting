2808daa10c629dbdd2c115f7c3703c2317d8f4cc
Ygjsz <yiyangdidi@gmail.com>
2026-03-03 22:07:48 +0800

    [Device] Fix Open License Info

--- app/src/main/java/com/jzzh/setting/device/LicenseInfo.java
+++ app/src/main/java/com/jzzh/setting/device/LicenseInfo.java
@@ -1,15 +1,12 @@
 package com.jzzh.setting.device;
 
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
@@ -24,50 +21,39 @@ public class LicenseInfo extends BaseActivity implements
         LoaderManager.LoaderCallbacks<File> {
     private static final String TAG = LicenseInfo.class.getSimpleName();
 
-    public static final String FILE_PROVIDER_AUTHORITY = "com.jzzh.setting.files";
     private static final String LICENSE_PATH = "/system/etc/NOTICE.html.gz";
+    public static final String FILE_PROVIDER_AUTHORITY = "com.jzzh.setting.files";
+
     private static final int LOADER_ID_LICENSE_HTML_LOADER = 0;
-    private WebView mInfoWebView;
-    private TextView mLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_device_info_license);
-        mInfoWebView = (WebView) findViewById(R.id.info_webview);
-        mLoading = findViewById(R.id.loading);
-        mInfoWebView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-                if (mInfoWebView.getContentHeight() > 0) {
-                    mLoading.setVisibility(View.GONE);
-                }
-                if (progress == 100) {
-                    //加载完成
-                    Log.d(TAG, "加载完成");
-                }
-            }
-        });
-
 
         File file = new File(LICENSE_PATH);
         if (isFileValid(file)) {
-            webViewShowHtmlFromUri(Uri.fromFile(file));
+            showHtmlFromUri(Uri.fromFile(file));
         } else {
             showHtmlFromDefaultXmlFiles();
         }
     }
 
-    private void showGeneratedHtmlFile(File generatedHtmlFile) {
-        if (generatedHtmlFile != null) {
-            webViewShowHtmlFromUri(getUriFromGeneratedHtmlFile(generatedHtmlFile));
-        } else {
-            Log.e(TAG, "Failed to generate.");
-//            showErrorAndFinish();
-        }
+    @Override
+    public Loader<File> onCreateLoader(int id, Bundle args) {
+        return new LicenseHtmlLoaderCompat(this);
     }
 
-    private void webViewShowHtmlFromUri(Uri uri) {
-        mInfoWebView.loadUrl(String.valueOf(uri));
+    @Override
+    public void onLoadFinished(Loader<File> loader, File generatedHtmlFile) {
+        showGeneratedHtmlFile(generatedHtmlFile);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<File> loader) {
+    }
+
+    private void showHtmlFromDefaultXmlFiles() {
+        getSupportLoaderManager().initLoader(LOADER_ID_LICENSE_HTML_LOADER, Bundle.EMPTY, this);
     }
 
     Uri getUriFromGeneratedHtmlFile(File generatedHtmlFile) {
@@ -75,33 +61,40 @@ public class LicenseInfo extends BaseActivity implements
                 generatedHtmlFile);
     }
 
+    private void showGeneratedHtmlFile(File generatedHtmlFile) {
+        if (generatedHtmlFile != null) {
+            showHtmlFromUri(getUriFromGeneratedHtmlFile(generatedHtmlFile));
+        } else {
+            Log.e(TAG, "Failed to generate.");
+            finish();
+        }
+    }
+
+    private void showHtmlFromUri(Uri uri) {
+        // Kick off external viewer due to WebView security restrictions; we
+        // carefully point it at HTMLViewer, since it offers to decompress
+        // before viewing.
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, "text/html");
+        intent.putExtra(Intent.EXTRA_TITLE, getString(R.string.setting_device_license));
+        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) {
+            intent.addFlags(
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                            | Intent.FLAG_GRANT_PREFIX_URI_PERMISSION);
+        }
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setPackage("com.android.htmlviewer");
+
+        try {
+            startActivity(intent);
+            finish();
+        } catch (ActivityNotFoundException e) {
+            Log.e(TAG, "Failed to find viewer", e);
+            finish();
+        }
+    }
+
     boolean isFileValid(final File file) {
         return file.exists() && file.length() != 0;
-    }
-
-    @NonNull
-    @Override
-    public Loader<File> onCreateLoader(int id, @Nullable Bundle args) {
-        mLoading.setVisibility(View.VISIBLE);
-        return new LicenseHtmlLoaderCompat(this);
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<File> loader, File data) {
-        showGeneratedHtmlFile(data);
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<File> loader) {
-    }
-
-    private void showHtmlFromDefaultXmlFiles() {
-        getSupportLoaderManager().initLoader(LOADER_ID_LICENSE_HTML_LOADER, Bundle.EMPTY, this);
-    }
-
-    @Override
-    protected void onPause() {
-        mInfoWebView.stopLoading();
-        super.onPause();
     }
 }
