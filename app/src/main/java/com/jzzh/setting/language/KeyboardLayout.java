263a675001148f9eaed1d1b5d62a10b189f95901
Ygjsz <yiyangdidi@gmail.com>
2026-03-27 10:29:28 +0800

    [Keyboard] Hide Google TTS in keyboard setting

--- app/src/main/java/com/jzzh/setting/language/KeyboardLayout.java
+++ app/src/main/java/com/jzzh/setting/language/KeyboardLayout.java
@@ -78,6 +78,10 @@ public class KeyboardLayout extends LinearLayout implements AdapterView.OnItemCl
     private List<InputMethodInfo> sortList(List<InputMethodInfo> list){
         List<InputMethodInfo> set = new ArrayList<>();
         for (InputMethodInfo info : list) { // 将DEFAULT_IMI_ID置于首位
+            if (info.getPackageName().equals("com.google.android.tts"))
+            {
+                continue;
+            }
             if (info.getId().equals(DEFAULT_IMI_ID))
                 set.add(0, info);
             else
